7a897ed0be14bc692f1b195f9e5ea80b66593dcd
deltaflyer4747 <andys@deltaflyer.cz>
2015-05-01 22:02:30 +0200

    Create Camera_get.py

---
+++ Camera_get.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# encoding: windows-1250
+#
+# Res Andy 
+
+import os, re, sys, time, socket
+
+
+camaddr = "192.168.42.1"
+camport = 7878
+
+srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+srv.connect((camaddr, camport))
+
+srv.send('{"msg_id":257,"token":0}')
+
+data = srv.recv(512)
+if "rval" in data:
+	token = re.findall('"param": (.+) }',data)[0]	
+data = srv.recv(512)
+if "rval" in data:
+	token = re.findall('"param": (.+) }',data)[0]	
+
+
+
+tosend = '{"msg_id":3,"token":%s}' %token 
+srv.send(tosend)
+
+while 1:
+	conf = srv.recv(4096)
+	if "param" in conf:
+		break
+
+conf = conf[37:]
+
+myconf = conf.split(",")
+
+for mytag in myconf:
+	print mytag
