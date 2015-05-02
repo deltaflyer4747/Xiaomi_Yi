83616a7133f3b7d7675fca70e422562a3415c68b
deltaflyer4747 <andys@deltaflyer.cz>
2015-05-02 10:42:57 +0200

    Stops video recording

---
+++ Camera_record_stop.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# encoding: windows-1250
+#
+# Res Andy 
+
+import os, re, sys, time, socket, settings
+
+
+srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+srv.connect((camaddr, camport))
+
+srv.send('{"msg_id":257,"token":0}')
+
+data = srv.recv(512)
+if "rval" in data:
+	token = re.findall('"param": (.+) }',data)[0]	
+else:
+	data = srv.recv(512)
+	if "rval" in data:
+		token = re.findall('"param": (.+) }',data)[0]	
+
+
+tosend = '{"msg_id":514,"token":%s}' %token
+srv.send(tosend)
+srv.recv(512)
