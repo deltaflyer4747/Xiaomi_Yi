02313c0a77196f128681eaaf3d116774dc4c4b45
deltaflyer4747 <andys@deltaflyer.cz>
2015-05-02 10:14:20 +0200

    Create Camera_video_stream.py
    Enables live video stream from camera to VLC or any other RTSP capable player

---
+++ Camera_video_stream.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# encoding: windows-1250
+#
+# Res Andy 
+
+import os, re, sys, time, socket
+
+
+camaddr = "192.168.42.1"
+camport = 7878
+
+srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+srv.connect((camaddr, camport))
+
+srv.send('{"msg_id":257,"token":0}')
+
+data = srv.recv(512)
+if "rval" in data:
+	token = re.findall('"param": (.+) }',data)[0]	
+else:
+	data = srv.recv(512)
+	if "rval" in data:
+		token = re.findall('"param": (.+) }',data)[0]	
+
+
+tosend = '{"msg_id":259,"token":%s,"param":"none_force"}' %token
+srv.send(tosend)
+srv.recv(512)
+print "Live webcam stream is now available."
+print 'Run VLC, select "Media"->"Open network stream" and open'
+print 'rtsp://%s/live' %camaddr
+print
+print "Press CTRL+C to end this streamer"
+
+while 1:
+	time.sleep(1)
